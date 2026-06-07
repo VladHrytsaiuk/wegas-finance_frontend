c13f117135bcf7fe816921c6aa51fae8cfa71ff8
Vlad Hrytsaiuk <vvgrytsayuk@gmail.com>
2026-06-07 15:51:44 +0300

    fix: correctly position transaction menu using Portal and dynamic coordinates

--- src/components/transactions/TransactionTableRow.tsx
+++ src/components/transactions/TransactionTableRow.tsx
@@ -1,4 +1,5 @@
-import { memo, useState, useRef, useEffect } from "react";
+import { memo, useState } from "react";
+import { createPortal } from "react-dom";
 import { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import {
@@ -14,6 +15,7 @@ import ConfirmDelete from "../ui/ConfirmDelete";
 
 import { TransactionIcon } from "./TransactionIcon";
 import { useTransactionItem } from "../../hooks/Transactions/useTransactionItem";
+import { useDropdownPosition } from "../../hooks/useDropdownPosition";
 
 import * as S from "./TransactionsTable.styles";
 
@@ -23,15 +25,13 @@ export const TransactionTableRow = memo(
     const location = useLocation();
 
     const [open, setOpen] = useState(false);
-    const ref = useRef(null);
 
-    useEffect(() => {
-      const close = (e) => {
-        if (ref.current && !ref.current.contains(e.target)) setOpen(false);
-      };
-      document.addEventListener("mousedown", close);
-      return () => document.removeEventListener("mousedown", close);
-    }, []);
+    const { triggerRef, menuRef, style } = useDropdownPosition(
+      open,
+      () => setOpen(false),
+      "right",
+      160,
+    );
 
     const display = useTransactionItem({
       transaction: tx,
@@ -127,30 +127,52 @@ export const TransactionTableRow = memo(
           </S.DesktopActions>
 
           {/* mobile */}
-          <S.MobileActions ref={ref} onClick={(e) => e.stopPropagation()}>
+          <S.MobileActions onClick={(e) => e.stopPropagation()}>
             {/* Обертаємо ВСЕ мобільне меню в Modal */}
             <Modal>
-              <S.MenuToggle onClick={() => setOpen((v) => !v)}>
+              <S.MenuToggle
+                ref={triggerRef as any}
+                onClick={() => setOpen((v) => !v)}
+              >
                 <HiEllipsisVertical size={20} />
               </S.MenuToggle>
 
-              {open && (
-                <S.MenuDropdown onClick={() => setOpen(false)}>
-                  {" "}
-                  {/* <--- ЗАКРИВАЄМО ПРИ БУДЬ-ЯКОМУ КЛІКУ */}
-                  <S.MenuItemLink
-                    to={`/transactions/${tx.id}/edit`}
-                    state={{ background: location }}
+              {open &&
+                createPortal(
+                  <S.MenuDropdown
+                    ref={menuRef}
+                    onClick={() => setOpen(false)}
+                    style={{
+                      position: "fixed",
+                      top: style.top !== undefined ? `${style.top}px` : "auto",
+                      bottom:
+                        style.bottom !== undefined
+                          ? `${style.bottom}px`
+                          : "auto",
+                      left:
+                        style.left !== undefined ? `${style.left}px` : "auto",
+                      right:
+                        style.right !== undefined ? `${style.right}px` : "auto",
+                      maxHeight: `${style.maxHeight}px`,
+                      transformOrigin: style.transformOrigin,
+                      zIndex: 99999,
+                      minWidth: "160px",
+                    }}
                   >
-                    <HiPencil size={16} /> {t("common:common.edit")}
-                  </S.MenuItemLink>
-                  <Modal.Open opens="delete">
-                    <S.MenuItemButton $variant="delete">
-                      <HiTrash size={16} /> {t("common:common.delete")}
-                    </S.MenuItemButton>
-                  </Modal.Open>
-                </S.MenuDropdown>
-              )}
+                    <S.MenuItemLink
+                      to={`/transactions/${tx.id}/edit`}
+                      state={{ background: location }}
+                    >
+                      <HiPencil size={16} /> {t("common:common.edit")}
+                    </S.MenuItemLink>
+                    <Modal.Open opens="delete">
+                      <S.MenuItemButton $variant="delete">
+                        <HiTrash size={16} /> {t("common:common.delete")}
+                      </S.MenuItemButton>
+                    </Modal.Open>
+                  </S.MenuDropdown>,
+                  document.body,
+                )}
 
               {/* Вікно підтвердження винесено СЮДИ, щоб воно не зникало при закритті меню */}
               <Modal.Window name="delete">
